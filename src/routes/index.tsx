9b8bcc223f9bd9d3c14e88479efa20983b8b54bc
Lancer 10 Evo / Lancer Team <mikhailduchenko@yandex.com>
2026-08-29 22:19:58 +0500

    Delete src directory

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { CrtApp } from "@/components/crt/CrtApp";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <CrtApp />;
-}
